b0062a79361c3aaf35d8a13b70990e01cf570524
Ana Mafla <amafla@hugeinc.com>
2022-05-17 11:20:33 -0500

    Setting project configuration with Parcel

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+const root = document.getElementById("root");
+
+let app = document.createElement("div");
+let title = document.createElement("h1");
+title.textContent = "Pokemon App Test";
+
+app.appendChild(title);
+
+root.appendChild(app);
